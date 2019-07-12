49194e1e7813b8bdf1eebcc8b214f208d6e0ad04
DPS0340 <jh001007@gmail.com>
2019-07-12 13:35:28 +0900

    firebase & main

--- app/src/main/java/com/dps0340/R6SDB/MainActivity.kt
+++ app/src/main/java/com/dps0340/R6SDB/MainActivity.kt
@@ -2,11 +2,20 @@ package com.dps0340.R6SDB
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val mAdView: AdView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adRequest = AdRequest.Builder().build()
+        mAdView!!.loadAd(adRequest)
     }
 }
