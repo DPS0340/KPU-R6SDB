b708a49f8d8196307f5a59fa5f1ba102f4b5540e
DPS0340 <jh001007@gmail.com>
2019-07-12 14:05:57 +0900

    .

--- app/src/main/java/com/dps0340/R6SDB/MainActivity.kt
+++ app/src/main/java/com/dps0340/R6SDB/MainActivity.kt
@@ -4,18 +4,19 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mAdView: AdView
 
-    private val mAdView: AdView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mAdView = findViewById(R.id.adView)
         val adRequest = AdRequest.Builder().build()
-        mAdView!!.loadAd(adRequest)
+        mAdView.loadAd(adRequest)
     }
 }
