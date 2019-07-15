9666226ef1209a7b8d3f9e73170bed379b8e5d8f
DPS0340 <jh001007@gmail.com>
2019-07-15 11:49:52 +0900

    .

---
+++ app/src/main/java/com/dps0340/R6SDB/describeActivity.kt
@@ -0,0 +1,12 @@
+package com.dps0340.R6SDB
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class describeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_describe)
+    }
+}
---
+++ app/src/main/java/com/dps0340/R6SDB/gunActivity.kt
@@ -0,0 +1,12 @@
+package com.dps0340.R6SDB
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class gunActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gun)
+    }
+}
