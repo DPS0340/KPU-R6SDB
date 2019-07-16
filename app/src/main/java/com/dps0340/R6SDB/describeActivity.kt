be54f26fdb8bf065e1f4fa36abe4d076476b2fbe
DPS0340 <jh001007@gmail.com>
2019-07-16 12:12:46 +0900

    액션바 타이틀 바꾸기

--- app/src/main/java/com/dps0340/R6SDB/describeActivity.kt
+++ app/src/main/java/com/dps0340/R6SDB/describeActivity.kt
@@ -8,5 +8,7 @@ class describeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_describe)
+        val actionBar = supportActionBar
+        actionBar!!.title = "상태"
     }
 }
