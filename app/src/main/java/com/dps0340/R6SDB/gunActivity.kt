43c32136910a1bd1cab205f6f0fad30c0d7c9533
DPS0340 <jh001007@gmail.com>
2019-07-16 12:12:12 +0900

    액션바 타이틀 바꾸기

--- app/src/main/java/com/dps0340/R6SDB/gunActivity.kt
+++ app/src/main/java/com/dps0340/R6SDB/gunActivity.kt
@@ -8,5 +8,7 @@ class gunActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gun)
+        val actionBar = supportActionBar
+        actionBar!!.title = "총"
     }
 }
--- app/src/main/java/com/dps0340/R6SDB/mapActivity.kt
+++ app/src/main/java/com/dps0340/R6SDB/mapActivity.kt
@@ -8,5 +8,7 @@ class mapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        val actionBar = supportActionBar
+        actionBar!!.title = "맵"
     }
 }
