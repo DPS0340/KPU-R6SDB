764b92d263444267453be171c366bc42baef868b
DPS0340 <jh001007@gmail.com>
2019-07-16 19:52:37 +0900

    gif 추가 & describe 레이아웃 개선

--- app/src/main/java/com/dps0340/R6SDB/describeActivity.kt
+++ app/src/main/java/com/dps0340/R6SDB/describeActivity.kt
@@ -2,6 +2,9 @@ package com.dps0340.R6SDB
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.media.MediaPlayer
+import android.widget.VideoView
+
 
 class describeActivity : AppCompatActivity() {
 
@@ -9,6 +12,10 @@ class describeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_describe)
         val actionBar = supportActionBar
-        actionBar!!.title = "상태"
+        actionBar!!.title = "슬레지"
+        val vV = findViewById<VideoView>(R.id.vv)
+        vV.setVideoPath("android.resource://" + packageName + "/" + R.raw.sledgeanimation)
+        vV.start()
+        vV.setOnCompletionListener(MediaPlayer.OnCompletionListener { vV.start() })
     }
 }
