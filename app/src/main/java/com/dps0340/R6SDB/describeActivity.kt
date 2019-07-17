54a536ce63b18cef6939c3b609d50d3971d42de5
DPS0340 <jh001007@gmail.com>
2019-07-17 16:27:32 +0900

    .

--- app/src/main/java/com/dps0340/R6SDB/describeActivity.kt
+++ app/src/main/java/com/dps0340/R6SDB/describeActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.media.MediaPlayer
 import android.widget.VideoView
 import android.R.attr.start
-
-
+import android.content.Intent
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_describe.*
+import kotlinx.android.synthetic.main.item.view.*
 
 
 class describeActivity : AppCompatActivity() {
@@ -30,8 +32,15 @@ class describeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_describe)
+        val res = resources
+        val int = intent
+        val id = int.getStringExtra("id")
+        Log.i("intent", "intent id is" + id)
+        val name = id + "_picture"
+        val resID = res.getIdentifier(name, "drawable", packageName)
+        picture.setImageResource(resID)
         val actionBar = supportActionBar
-        actionBar!!.title = "슬레지"
+        actionBar!!.title = id
         vV = findViewById<VideoView>(R.id.vv)
         vV.setVideoPath("android.resource://" + packageName + "/" + R.raw.sledgeanimation)
         vV.start()
