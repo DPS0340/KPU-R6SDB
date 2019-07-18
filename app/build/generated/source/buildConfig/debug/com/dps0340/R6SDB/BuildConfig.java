b7471958db8994d62d964b2b8c7e03bfbbf893f8
DPS0340 <jh001007@gmail.com>
2019-07-18 21:11:28 +0900

    총기류

--- app/build/generated/source/buildConfig/debug/com/dps0340/R6SDB/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/dps0340/R6SDB/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.dps0340.R6SDB";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "2.0";
 }
