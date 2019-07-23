9cb23d30a8689fdf321d9e32601059d67b58e9b6
DPS0340 <jh001007@gmail.com>
2019-07-23 14:35:33 +0900

    dynamicJSONTextView add. not tested yet

--- app/src/main/java/com/dps0340/R6SDB/describeActivity.kt
+++ app/src/main/java/com/dps0340/R6SDB/describeActivity.kt
@@ -2,63 +2,116 @@ package com.dps0340.R6SDB
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.media.MediaPlayer
-import android.widget.VideoView
-import android.R.attr.start
-import android.content.Intent
-import android.media.AudioManager
 import android.os.Build
 import android.support.annotation.RequiresApi
 import android.util.Log
-import android.widget.MediaController
-import kotlinx.android.synthetic.main.activity_describe.*
-import kotlinx.android.synthetic.main.item.view.*
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import org.json.JSONArray
+import org.json.JSONObject
 
 
 class describeActivity : AppCompatActivity() {
-
-    lateinit var vV: VideoView
-    var seekTime: Int = 0
-
-    public override fun onPause() {
-        super.onPause()
-        if (vV != null && vV.isPlaying()) {
-            vV.pause()
-            seekTime = vV.getCurrentPosition()
-        }
-    }
-    public override fun onResume() {
-        super.onResume()
-        vV.start()
-        vV.seekTo(seekTime)
-    }
-
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_describe)
+
+
+        val guninfo = JSONObject(
+            R.string.guninfo.toString()
+        )
+        val opinfo = JSONObject(
+            R.string.opinfo.toString()
+        )
+
         val res = resources
         val int = intent
+
         val id = int.getStringExtra("id")
         Log.i("intent", "intent id is" + id)
-        val name = id + "_picture"
-        val name2 = id + "_logo"
-        var resID = res.getIdentifier(name, "drawable", packageName)
-        picture.setImageResource(resID)
-        resID = res.getIdentifier(name2, "drawable", packageName)
-        logo.setImageResource(resID)
+
+        val isgun = guninfo.has(id)
+        val isop = opinfo.has(id)
+
+        val root = findViewById<LinearLayout>(R.id.root)
+
+
+        if(isgun) {
+            val name = id
+            val resID = res.getIdentifier(name, "drawable", packageName)
+            val picture = ImageView(this)
+            picture.setImageResource(resID)
+            root.addView(picture)
+        } else if(isop) {
+            val name = id + "_picture"
+            val name2 = id + "_logo"
+            var resID = res.getIdentifier(name, "drawable", packageName)
+            val picture = ImageView(this)
+            picture.setImageResource(resID)
+            resID = res.getIdentifier(name2, "drawable", packageName)
+            val logo = ImageView(this)
+            logo.setImageResource(resID)
+            root.addView(picture)
+            root.addView(logo)
+        }
+
+        if(isgun) {
+            dynamicJSONTextView(guninfo, root)
+        }
+
+        if(isop) {
+            dynamicJSONTextView(opinfo, root)
+        }
         val actionBar = supportActionBar
         actionBar!!.title = id
-        vV = findViewById<VideoView>(R.id.vv)
-        vV.setVideoPath("android.resource://" + packageName + "/" + R.raw.sledgeanimation)
-        vV.setAudioFocusRequest(AudioManager.AUDIOFOCUS_NONE)
-        vV.setOnPreparedListener(MediaPlayer.OnPreparedListener {
-            it.setVolume(0f, 0f)
-            vV.start()
-        })
-        vV.setOnCompletionListener(MediaPlayer.OnCompletionListener {
-            it.setVolume(0f, 0f)
-            vV.start()
-        })
+    }
+
+    fun dynamicJSONTextView(obj: JSONObject, root: LinearLayout) {
+        val keys = obj.names()
+        val usingLL = LinearLayout(this)
+        for (i in 0..obj.length() - 1) {
+            val key = keys.getString(i)
+            val value = obj.getString(key)
+            var isjsonobj: Boolean
+            var isjsonarr: Boolean
+            try {
+                JSONObject(value)
+                isjsonobj = true
+            } catch (e: Error) {
+                isjsonobj = false
+            }
+            try {
+                JSONArray(value)
+                isjsonarr = true
+            } catch (e: Error) {
+                isjsonarr = false
+            }
+            if (isjsonobj) {
+                val LL = LinearLayout(this)
+                dynamicJSONTextView(JSONObject(value), LL)
+                usingLL.addView(LL)
+                usingLL.invalidate()
+            } else if(isjsonarr) {
+                val LL = LinearLayout(this)
+                val arr = JSONArray(value)
+                for(i in 0..arr.length() - 1) {
+                    val textV = TextView(this)
+                    textV.setText(arr[i].toString())
+                    LL.addView(LL)
+                    LL.invalidate()
+                }
+                usingLL.addView(LL)
+                usingLL.invalidate()
+            } else {
+                val textV = TextView(this)
+                textV.setText(key + value)
+                usingLL.addView(textV)
+                usingLL.invalidate()
+            }
+        }
+        root.addView(usingLL)
+        root.invalidate()
     }
 }
